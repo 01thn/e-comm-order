b6a1d4d108f1da6b34e26d9b6f2dd48a4a2f6030
Tikhon Novitskiy <Novitskiy@lanit.ru>
2024-06-17 14:44:26 +0300

    Init commit

---
+++ src/main/kotlin/by/ecomm/ecommorder/ECommOrderApplication.kt
@@ -0,0 +1,11 @@
+package by.ecomm.ecommorder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ECommOrderApplication
+
+fun main(args: Array<String>) {
+    runApplication<ECommOrderApplication>(*args)
+}
---
+++ src/test/kotlin/by/ecomm/ecommorder/ECommOrderApplicationTests.kt
@@ -0,0 +1,13 @@
+package by.ecomm.ecommorder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ECommOrderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
